5df9a00e7ca46f58bae132030e84fa65a7b98cf7
David Barranco <dbarranco@bitnami.com>
2017-02-23 21:50:46 +0100

    Added pseudoComputeDelay

--- computeDelay.go
+++ computeDelay.go
@@ -1,11 +1,26 @@
 package main
 
-func computeDelay(Endpoints, s CacheServer) {
-    delay int64
+func computeDelay(Endpoints, s CacheServer, r Request) {
+	var delay int64
 
-    for index := 0; index < count; index++ {
-        
-    }
+	for index := 0; index < len(Endpoints); index++ {
+		actualEndpoint = Endpoints[index]
+		if r.Re != index { 
+        //Este endpoint no hace esa request
+            if actualEndpoint.CacheServers[index].C == s.C  {
+                 //Que el endpoint este enganchado al CacheServer       
+                 delay=delay+actualEndpoint.CacheServers[index].Lc
+			}
+            else{
+                delay=delay+actualEndpoint.LD
+            }
 
+		}
 
+	}
+
+//delay completo de la request ejecutada por ese CacheServer
+return delay
 }
+
+
